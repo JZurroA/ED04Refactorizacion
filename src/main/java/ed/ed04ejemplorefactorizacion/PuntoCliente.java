6b4d2ce23e49f25b176d27f2649a64fccdae963f
JZurroA <152436896+JZurroA@users.noreply.github.com>
2024-05-03 20:02:54 +0200

    Update PuntoCliente.java

--- src/main/java/ed/ed04ejemplorefactorizacion/PuntoCliente.java
+++ src/main/java/ed/ed04ejemplorefactorizacion/PuntoCliente.java
@@ -1,45 +1,25 @@
-package ed.ed04ejemplorefactorizacion;
 /*
-   Programa cliente que utiliza la clase Punto definida en el fichero Punto.java
-   Se debe compilar primero el fichero Punto.java y generar Punto.class
-   para que el programa funcione
-*/
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ed.ed04refactorizacion;
 
 /**
  *
  * @author Jabier Zurro Aduriz
  */
 public class PuntoCliente {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
    
-      ClasePunto p1 = crearPunto(); 
+        ClasePunto p1 = new ClasePunto(2, 6); 
       
-      modificarPunto(p1, 2, 6);
+        // Punto 2
+        ClasePunto p2 = new ClasePunto(4, 9);            
+        visualizarPunto(p1, p2);
       
-      // Punto 2
-      ClasePunto p2 = crearPunto();
-      
-      modificarPunto(p2, 4, 9);
-            
-      visualizarPunto(p1, p2);
-      
-      p1.moverPunto(5, -7);
-      visualizarPunto(p1, p2);
-   }
-
-    public static void modificarPunto(ClasePunto p1, int coordX, int coordY) {
-        System.out.println("Modificamos las coordenadas de un punto");
-        p1.setX(coordX);
-        p1.setY(coordY);
+        p1.moverPunto(5, -7);
+        visualizarPunto(p1, p2);
     }
-
-    public static ClasePunto crearPunto() {
-        // Punto 1
-        System.out.println(MENSAJE_CREAR);
-        ClasePunto p1 = new ClasePunto();
-        return p1;
-    }
-    public static final String MENSAJE_CREAR = "Creamos un punto";
 
     public static void visualizarPunto(ClasePunto p1, ClasePunto p2) {
         // Se ejecutan los mÃ©todos de la clase Punto y
